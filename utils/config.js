d7b84862397f9e87c21faba81387bdda326dfa83
VKovrigina <v.kovrigina.2001@yandex.ru>
2020-11-06 08:40:32 +0300

    feat: add mongo test address

--- utils/config.js
+++ utils/config.js
@@ -1,3 +1,4 @@
 const JWT_SECRET_DEV = 98778765677;
 const MONGO_ADDRESS = 'mongodb://localhost:27017/newsExplorer';
-module.exports = { JWT_SECRET_DEV, MONGO_ADDRESS };
+const MONGO_TEST_ADDRESS = 'mongodb://localhost:27017/testMongo';
+module.exports = { JWT_SECRET_DEV, MONGO_ADDRESS, MONGO_TEST_ADDRESS };
