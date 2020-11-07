601d84cf382f4e3d6980aa6645c42ffc07c3dbce
VKovrigina <v.kovrigina.2001@yandex.ru>
2020-11-07 12:26:36 +0300

    feat:  mongoose connect in server.js

--- app.js
+++ app.js
@@ -1,22 +1,13 @@
 const express = require('express');
-const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const helmet = require('helmet');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const router = require('./routes/index');
 const { limiter } = require('./utils/constants');
-const { MONGO_ADDRESS } = require('./utils/config');
 const handlerErrors = require('./middlewares/handlerErrors');
 
 const app = express();
-
-mongoose.connect(MONGO_ADDRESS, {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-  useUnifiedTopology: true,
-});
 
 app.use(cors());
 app.use(helmet());
--- server.js
+++ server.js
@@ -1,5 +1,14 @@
+const mongoose = require('mongoose');
 const app = require('./app.js');
+const { MONGO_ADDRESS } = require('./utils/config');
 
 const { PORT = 3000 } = process.env;
+
+mongoose.connect(MONGO_ADDRESS, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+});
 
 app.listen(PORT);
